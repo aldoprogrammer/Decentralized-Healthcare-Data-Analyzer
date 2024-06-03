7936158d6a807b112671b9d8bb6011f95d162c53
aldoprogrammer <aldobesma@gmail.com>
2024-06-03 20:30:57 +0700

    feat: Add PrintInvoice page to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Medichine from './pages/Medichine';
-
+import PrintInvoice from './pages/PrintInvoice';
 
 function App() {
   return (
@@ -12,9 +12,10 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/medicine" element={<Medichine />} />
+        <Route path="/print-invoice" element={<PrintInvoice />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
