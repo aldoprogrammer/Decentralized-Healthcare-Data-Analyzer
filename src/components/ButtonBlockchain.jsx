bd252b052df2174712982965f80e86acf99a3e8a
aldoprogrammer <aldobesma@gmail.com>
2024-06-03 20:32:37 +0700

    Refactor ButtonBlockchain component for improved readability and maintainability

--- src/components/ButtonBlockchain.jsx
+++ src/components/ButtonBlockchain.jsx
@@ -14,14 +14,14 @@ const ButtonBlockchain = () => {
         }, 3000);
     }
 
-  return (
-    <button 
-    onClick={handleBlockchain} 
-    className="px-5 py-2 bg-green-500 text-white rounded transition duration-300 ease-in-out hover:bg-green-700 hover:shadow-lg transform hover:-translate-y-1 w-[190px] flex items-center justify-center"
->
-     {loading ? <HashLoader color='#ffffff' size={20} /> : "Store to Blockchain"}
-</button>
-  )
+    return (
+        <button
+            onClick={handleBlockchain}
+            className="px-5 py-2 bg-green-500 text-white rounded transition duration-300 ease-in-out hover:bg-green-700 hover:shadow-lg transform hover:-translate-y-1 w-[190px] flex items-center justify-center"
+        >
+            {loading ? <HashLoader color='#ffffff' size={20} /> : "Store to Blockchain"}
+        </button>
+    )
 }
 
 export default ButtonBlockchain
